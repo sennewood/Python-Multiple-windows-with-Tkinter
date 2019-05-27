ab09f2ab788a84a11cbbbe79770da803fc23d784
Sebastian <sennewood@users.noreply.github.com>
2019-05-27 17:36:01 +0200

    Create pageStart.py

---
+++ splitted-file/pageStart.py
@@ -0,0 +1,36 @@
+# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+# License: http://creativecommons.org/licenses/by-sa/3.0/	
+# https://pythonprogramming.net/change-show-new-frame-tkinter/?completed=/passing-functions-parameters-tkinter-using-lambda/
+
+import tkinter as tk
+from tkinter import ttk
+
+import pageApplication as App
+import pageOne as PageOne
+import pageTwo as PageTwo
+
+LARGE_FONT = ("Verdana", 12)
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        
+        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        self.label2 = ttk.Label(self, text=controller.getVUE(), font=LARGE_FONT)
+        self.label2.pack(pady=10,padx=10)
+
+        button = ttk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button2 = ttk.Button(self, text="Visit Page 2",
+                            command=lambda: controller.show_frame(PageTwo))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="+1",
+                            command=lambda: controller.raiseVUE(StartPage))
+        button3.pack()
